c3b6981b651b31fd60f2b66c20bf67322cb7082b
Alexsey <tolalur@gmail.com>
2019-03-25 17:43:23 +0300

    интеграция писем в шаблоны

--- save.go
+++ save.go
@@ -6,17 +6,47 @@ import (
 	"html/template"
 	"os"
 )
+type viewData struct{
+ 
+    OpenIETag template.HTML
+	CloseIETag template.HTML
+	Version int
+}
 
 func main() {
 	buf := new(bytes.Buffer)
-	tmpl, err := template.ParseFiles("templates/index.html", "templates/head.html")
+
+	htmlFiles := []string{
+            "templates/index.html", 
+            "templates/show_in_browser.html",
+            "templates/logo.html",
+            "templates/header_menu.html",
+            "templates/content.html",
+            "templates/social.html",
+            "templates/footer_menu.html",
+            "templates/footer_phone.html",
+            "templates/footer_unsubscribe.html",
+            "templates/not_an_offer.html",
+            "templates/copy_right.html"}
+			
+	tmpl, err := template.ParseFiles(htmlFiles...)
 
 	if err != nil {
 		fmt.Println("Ошибка парсинга")
 		fmt.Printf("", err)
 	}
 
-	tmpl.ExecuteTemplate(buf, "index", "");
+	data := viewData{
+			OpenIETag: `<!--[if (gte mso 9)|(IE)]>
+          					<table align="center" cellspacing="0" cellpadding="0" border="0" width="600" style="width: 600px;">
+          						<tr>
+          							<td style="padding-top: 0px; padding-bottom: 0px; padding-right: 0px; padding-left: 0px; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px;">
+        				<![endif]-->`,
+			CloseIETag: "<!--[if (gte mso 9)|(IE)]></td></tr></table><![endif]-->",
+			Version: 274,
+		}
+		
+	tmpl.ExecuteTemplate(buf, "index", data);
 	templateStringx := buf.String();
 
 	file, err := os.Create("./static/index.html")
--- server.go
+++ server.go
@@ -8,31 +8,42 @@ import (
 
 type viewData struct{
  
-    openIETag template.HTML
-    closeIETag string
+    OpenIETag template.HTML
+	CloseIETag template.HTML
+	Version int
 }
 
 func main() {
 
 	// отдаем статику
 	fs := http.FileServer(http.Dir("static"))
-	fmt.Println(fs)
 	http.Handle("/", fs)
 
 	http.HandleFunc("/dev", func(w http.ResponseWriter, r *http.Request) {
 		htmlFiles := []string{
             "templates/index.html", 
-            "templates/show_in_browser.html"}
+            "templates/show_in_browser.html",
+            "templates/logo.html",
+            "templates/header_menu.html",
+            "templates/content.html",
+            "templates/social.html",
+            "templates/footer_menu.html",
+            "templates/footer_phone.html",
+            "templates/footer_unsubscribe.html",
+            "templates/not_an_offer.html",
+            "templates/copy_right.html"}
 
         tmpl, _ := template.ParseFiles(htmlFiles...)
         data := viewData{
-            openIETag: "<!--[if (gte mso 9)|(IE)]><table align=\"center\" cellspacing=\"0\" cellpadding=\"0\" border=\"0\" width=\"600\" style=\"width: 600px;\"><tr><td style=\"padding-top: 0px; padding-bottom: 0px; padding-right: 0px; padding-left: 0px; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px;\"><![endif]-->",
-            closeIETag: "<!--[if (gte mso 9)|(IE)]></td></tr></table><![endif]-->",
+			OpenIETag: `<!--[if (gte mso 9)|(IE)]>
+          					<table align="center" cellspacing="0" cellpadding="0" border="0" width="600" style="width: 600px;">
+          						<tr>
+          							<td style="padding-top: 0px; padding-bottom: 0px; padding-right: 0px; padding-left: 0px; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px;">
+        				<![endif]-->`,
+			CloseIETag: "<!--[if (gte mso 9)|(IE)]></td></tr></table><![endif]-->",
+			Version: 274,
         }
-        // data := viewData{
-        //     openIETag: "Привет",
-        //     closeIETag: "Привет2",
-        // }
+
 		tmpl.ExecuteTemplate(w, "index", data)
 	})
 
