b112c01fffd8b6491d671e4d46125c92b579db54
Alexsey <tolalur@gmail.com>
2019-03-26 23:06:21 +0300

    настройки в отдельном файле

--- server.go
+++ server.go
@@ -4,14 +4,9 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"./config"
 )
 
-type viewData struct{
- 
-    OpenIETag template.HTML
-	CloseIETag template.HTML
-	Version int
-}
 
 func main() {
 
@@ -20,29 +15,12 @@ func main() {
 	http.Handle("/", fs)
 
 	http.HandleFunc("/dev", func(w http.ResponseWriter, r *http.Request) {
-		htmlFiles := []string{
-            "templates/index.html", 
-            "templates/show_in_browser.html",
-            "templates/logo.html",
-            "templates/header_menu.html",
-            "templates/content.html",
-            "templates/social.html",
-            "templates/footer_menu.html",
-            "templates/footer_phone.html",
-            "templates/footer_unsubscribe.html",
-            "templates/not_an_offer.html",
-            "templates/copy_right.html"}
+		
+		htmlFiles := config.Templates()
+		data := config.GetData()
 
         tmpl, _ := template.ParseFiles(htmlFiles...)
-        data := viewData{
-			OpenIETag: `<!--[if (gte mso 9)|(IE)]>
-          					<table align="center" cellspacing="0" cellpadding="0" border="0" width="600" style="width: 600px;">
-          						<tr>
-          							<td style="padding-top: 0px; padding-bottom: 0px; padding-right: 0px; padding-left: 0px; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px;">
-        				<![endif]-->`,
-			CloseIETag: "<!--[if (gte mso 9)|(IE)]></td></tr></table><![endif]-->",
-			Version: 274,
-        }
+
 
 		tmpl.ExecuteTemplate(w, "index", data)
 	})
