2fde7e2fc78d485651a52c315ad835566c3c217c
Alexsey <tolalur@gmail.com>
2019-03-27 10:24:11 +0300

    фикс подвала

--- save.go
+++ save.go
@@ -43,7 +43,7 @@ func main() {
           							<td style="padding-top: 0px; padding-bottom: 0px; padding-right: 0px; padding-left: 0px; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px;">
         				<![endif]-->`,
 			CloseIETag: "<!--[if (gte mso 9)|(IE)]></td></tr></table><![endif]-->",
-			Version: 264,
+			Version: 274,
 		}
 		
 	tmpl.ExecuteTemplate(buf, "index", data);
